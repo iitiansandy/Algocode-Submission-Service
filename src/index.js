bca65b256fce0316206beb44fdb8e84de6f45f70
iitiansandy <sandeep.sid.kumar10@gmail.com>
2024-06-06 15:54:22 +0530

    db connected

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const fastify = require('fastify')({ logger: true });
 const app = require('./app');
+const { connectToDB } = require('./config/db.config');
 const serverConfig = require('./config/serverConfig');
 
 // fastify.get('/ping', (req, res) => {
@@ -11,10 +12,12 @@ fastify.register(app);
 
 
 
-fastify.listen({ port: serverConfig.port }, (err) => {
+fastify.listen({ port: serverConfig.port }, async (err) => {
     if (err) {
         fastify.log.error(err);
         process.exit(1);
     };
+
+    await connectToDB();
     console.log('Server is running on port', serverConfig.port);
 })
